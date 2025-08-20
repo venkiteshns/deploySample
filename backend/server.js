f3185e409cfacaedb42aae247dbb4b2553085e3e
venkiteshns <software.venkitesh.in@gmail.com>
2025-08-20 22:39:54 +0530

    url back change

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
 
 app.get("/getData",(req,res)=>{
     res.send("Venkitesh N S YOU ARE GREAT!!")
